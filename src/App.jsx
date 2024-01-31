9d42d0092641db9a65541eaf14fe45e6b369cdc0
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-01-31 20:09:11 +0500

    Setting up the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import { Outlet } from "react-router-dom"
 
 function App() {
   return (
     <>
-
+      <div className="w-full">
+        Navbar 
+        <Outlet />
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Home ,ExerciseDetail} from './components'
 
 const router = createBrowserRouter([
   createRoutesFromElements(
     <Route path="/" element={<App />} >
-      
+      <Route path='/' element={<Home />}/>
+      <Route path='exercise/:id' element={<ExerciseDetail />}/>
     </Route>
   )
 ])
