2c8fce13c0961670c5e397fc9f8462b06e9fa971
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-02-05 00:39:16 +0500

    Creating ExerciseDetail Section

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,5 @@ export {default as Footer} from './Footer/Footer'
 export {default as Hero} from './Header/Hero'
 export {default as SearchExercises} from './SearchExercises/SearchExercises'
 export {default as HorizontalScrollbar} from './HorizontalScrollbar/HorizontalScrollbar'
+export {default as Detail} from './ExerciseDetails/Detail'
 export default Exercises
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchData } from '../FetchData/fetchData'
+import { Detail } from '../components'
 
 const ExerciseDetail = () => {
+  const { id } = useParams()
+  const [ExerciseDetail, setExerciseDetail] = useState({
+    
+  })
+
+  useEffect(() => {
+    fetchData(`exercises/exercise/${id}`).then(data => {
+      setExerciseDetail(data)
+    })
+  }, [id])
+
+
+
   return (
-    <div>ExerciseDetail</div>
+    <div>
+      <Detail exerciseDetail={ExerciseDetail}/>
+    </div>
   )
 }
 
